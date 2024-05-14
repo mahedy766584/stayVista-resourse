2864f582ca3c07d5524f4c4c3d620b9148017a50
mahedy766584 <108580163+mahedy766584@users.noreply.github.com>
2024-05-15 05:30:33 +0600

    new file updated

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-}
+  apiKey: "AIzaSyBiB-s3Az_g1rUfqcR7X-6LS5l9qfG7-RY",
+  authDomain: "stayvista-f44f4.firebaseapp.com",
+  projectId: "stayvista-f44f4",
+  storageBucket: "stayvista-f44f4.appspot.com",
+  messagingSenderId: "76930740692",
+  appId: "1:76930740692:web:ff187c8a61fc8b5a2d2641"
+};
 
 export const app = initializeApp(firebaseConfig)
